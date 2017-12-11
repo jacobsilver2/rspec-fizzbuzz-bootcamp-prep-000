75c601fe61495e6e85d65e7b281692acab5f381f
Jacob Silver <jacobsilver2@mac.com>
2017-12-11 23:46:58 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,8 +2,8 @@ def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
     "Fizz" # Go fizz
   end
-  if int % 5 == 0 # if the number int is divisible by 3
-    "Buzz" # Go fizz
+  if int % 5 == 0 
+    "Buzz"
   end
 
 end
